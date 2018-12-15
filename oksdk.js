4bfc25671819ed9b6286e9a888318a117804ba44
Maxim Egorichev <maxim.egorichev@corp.mail.ru>
2018-12-15 22:02:31 +0300

    window

--- oksdk.js
+++ oksdk.js
@@ -262,7 +262,7 @@
         var frameElement =
         "'style='position: absolute; left: 0px; top: 0px; background-color: white; z-index: 9999;' src='"
         + query
-        + "'; width='100%' height='100%' frameborder='0'></iframe>"
+        + "'; width='100%' height='100%' frameborder='0'></iframe>";
         element = window.document.getElementById(frameId);
         element.innerHTML = frameElement;
         element.style.display = 'block';
