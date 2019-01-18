8232f970a68037190f1cc0d44872194aa13d3760
Maxim Egorichev <maxim.egorichev@corp.mail.ru>
2019-01-18 18:21:37 +0300

    ads

--- oksdk.js
+++ oksdk.js
@@ -21,15 +21,15 @@
     };
 
     var ads_widget_style = {
-        border: 0;
-        position: fixed;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        width: "100%";
-        height: "100%";
-        z-index: 1000;
+        border: 0,
+        position: "fixed",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        z-index: 1000
     }
 
     var sdk_success = nop;
