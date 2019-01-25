ccdc7a2e880eb72bb4ac0a5c2b5ffcfc4ea227e7
Maxim Egorichev <maxim.egorichev@corp.mail.ru>
2019-01-25 17:34:27 +0300

    test payment header

--- oksdk.js
+++ oksdk.js
@@ -289,7 +289,7 @@
         }
         var paymentHeader = "";
         if (isLaunchedInOKAndroidWebView()) {
-            paymentHeader += '<div class="head_t" style="height: 42px; display: block; width: 100%; text-align: center; margin: 0; padding: 0; font-family: Arial,Helvetica,sans-serif; color: #fff; overflow: hidden; position: relative; background-color: #ed812b; z-index: 1;">Покупка в игре<div class="head_t_small"></div></div><a id="ret" href="https://m.ok.ru/app/okpaymentexample?custom_args=payment%3Dcancel"><div class="head_exit"></div></a>';
+            paymentHeader += '<div class="head_t" style="height: 42px; display: block; width: 100%; text-align: center; margin: 0; padding: 0; font-family: Arial,Helvetica,sans-serif; color: #fff; overflow: hidden; position: relative; background-color: #ed812b; z-index: 1;">Покупка в игре<div class="head_t_small"></div></div><a id="ret" href="https://m.ok.ru/app/okpaymentexample?custom_args=payment%3Dcancel"><div class="head_exit" style="position: absolute; top: 0; width: 42px; height: 42px; display: inline-block; cursor: pointer;"></div></a>';
         }
         frameContainer.innerHTML = paymentHeader + frameElement;
         frameContainer.style.display = "block";
